e84587206f9c04ec065be720f034cd1e5015df9c
Norman <normanndot@gmail.com>
2020-10-04 11:19:52 +0530

    languageColor & language properties are not a mandatory ones as, at times those are missing from the response. Hence made them as optional.

--- git-trends/git-trends/Git-Trends/GitTrendingList/GitTrendingModel.swift
+++ git-trends/git-trends/Git-Trends/GitTrendingList/GitTrendingModel.swift
@@ -8,17 +8,18 @@
 import Foundation
 
 // MARK: - TrendingRepos
-struct TrendingRepos: Codable {
+struct TrendingRepos: Decodable {
     let author, name: String
     let avatar: String
     let url: String
-    let description, language, languageColor: String
+    let description: String
+    let language, languageColor: String?
     let stars, forks, currentPeriodStars: Int
     let builtBy: [BuiltBy]
 }
 
 // MARK: - BuiltBy
-struct BuiltBy: Codable {
+struct BuiltBy: Decodable {
     let href, avatar: String
     let username: String
 }
